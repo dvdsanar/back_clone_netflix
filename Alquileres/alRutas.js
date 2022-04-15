8ffab59312599352bb4b750736e58d52127d1666
David S <dvdsanar@gmail.com>
2022-04-15 09:21:41 +0200

    arreglo ruta get por ID de usuario

--- Alquileres/alRutas.js
+++ Alquileres/alRutas.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require("./alController.js");
 
 router.get("/", controller.getAlquileres);
-router.get("/usuario", controller.getAlquilerIdUsuario);
+router.get("/:id", controller.getAlquilerIdUsuario);
 router.post("/", controller.postAlquiler);
 router.patch("/:id", controller.patchAlquiler);
 router.delete("/:id", controller.deleteAlquiler);
